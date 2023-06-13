07f3f953d31b727f0aa1a079cbd76855ab1cf7da
Joaquin Blanco <blancojoaquin77@gmail.com>
2023-06-13 01:14:03 -0300

    update test

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,12 +1,12 @@
 import { render, screen } from "@testing-library/react";
-import Home from "../pages/index";
+import Home from "@/pages/index";
 import "@testing-library/jest-dom";
 
 describe("Home", () => {
   test("renders a heading", async () => {
     render(<Home />);
-    const element = await screen.findByText("Comienza a crear");
-    // const element = screen.getByText("for testing");
-    expect(element).toHaveTextContent("comenzar a crear");
+    const elements = await screen.findAllByText("Comienza a crear");
+    const element = elements[0];
+    expect(element).toBeVisible();
   });
 });
