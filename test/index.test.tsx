3983aa4a04c902dbf13c8d6bf5a17b647baf73fe
Joaquin Blanco <blancojoaquin77@gmail.com>
2023-06-12 23:23:18 -0300

    update dependencies

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -3,9 +3,10 @@ import Home from "../pages/index";
 import "@testing-library/jest-dom";
 
 describe("Home", () => {
-  test("renders a heading", () => {
+  test("renders a heading", async () => {
     render(<Home />);
-    const element = screen.getByText("for testing");
-    expect(element).toHaveTextContent("for testing");
+    const element = await screen.findByText("Comienza a crear");
+    // const element = screen.getByText("for testing");
+    expect(element).toHaveTextContent("comenzar a crear");
   });
 });
